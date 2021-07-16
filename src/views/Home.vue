19930813846925fb41745f06d67cfd5e33724c3f
Władysław Reginiewicz <wladyslaw.reginiewicz@gmail.com>
2021-07-16 13:45:46 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,53 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- we need event listener only for value in it from emit -->
+    <FilterNav @filterChange="current =$event" :current="current"/>
+    <div v-if="projects.length">
+    <div v-for="project in projects" :key="project.id">
+
+<!-- adding custom event for update in case of deletion  -->
+<SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import SingleProject from '../components/SingleProject.vue'
+import FilterNav from '../components/FilterNav.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  components: {SingleProject, FilterNav},
+  data(){
+    return{
+      projects:[],
+      current: 'all'
+    }
+  },
+
+  mounted(){
+    fetch('http://localhost:3000/projects')
+    .then(res=>res.json())
+    .then(data =>this.projects = data )
+    .catch(err =>console.log(err.message))
+  },
+  methods:{
+    // creating method for deletion project.id  from an array projects 
+    handleDelete(id){
+      this.projects = this.projects.filter((project) =>{
+        // if false (project.id == id) - then item will be deleted from an array
+        return project.id !== id;
+      })
+
+    },
+    handleComplete(id){
+      let p =this.projects.find(project =>{
+        return project.id == id
+      })
+      p.complete = !p.complete
+    }
   }
+
 }
 </script>
